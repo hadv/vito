380fdaab7655991216de9d5663ab582e0c5ccd17
prpeh <prpeh@proton.me>
2025-03-01 22:11:52 +0700

    only safe wallet owners can use the tx mode

--- server/src/safe/safe.module.ts
+++ server/src/safe/safe.module.ts
@@ -1,11 +1,9 @@
 import { Module } from '@nestjs/common';
-import { HttpModule } from '@nestjs/axios';
 import { SafeController } from './safe.controller';
 import { SafeService } from './safe.service';
 import { SafeGateway } from './safe.gateway';
 
 @Module({
-  imports: [HttpModule],
   controllers: [SafeController],
   providers: [SafeService, SafeGateway],
 })
